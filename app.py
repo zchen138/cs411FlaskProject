4cdf5eb3a224d578b9ba9a8104dce73c1f40d2cc
Zuyi Chen <zchen138@illinois.edu>
2018-03-23 14:24:55 -0500

    extra push

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from flask import request, redirect, render_template, url_for
 import MySQLdb
 
 app = Flask(__name__)
-conn = 
+conn = MySQLdb.connect(host="localhost", user="root", password="pass", db=)
