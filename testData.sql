fef73600cdca24774902e0bc2bf5412ad8e8edb6
bryant100000 <bhzhao2@illinois.edu>
2018-03-24 16:31:23 -0500

    data sql

---
+++ testData.sql
@@ -0,0 +1,16 @@
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('1', 'a', '1997', '123', 'comedy');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('2', 'b', '1999', '124', 'action');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('3', 'c', '1997', '125', 'comedy');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('4', 'd', '1997', '126', 'action');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('5', 'e', '1997', '127', 'romance');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('6', 'f', '1997', '128', 'horror');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('7', 'g', '1997', '129', 'romance');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('8', 'h', '1997', '130', 'comedy');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('9', 'i', '1997', '132', 'comedy');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('10', 'j', '1997', '133', 'comedy');
+INSERT INTO heroku_37da5348cc1f7c7.movieinfo (movieid, title, releaseYear, runtime, genre) VALUES ('11', 'k', '1997', '143', 'comedy');
+
+INSERT INTO heroku_37da5348cc1f7c7.rated (userid, movieid, rating) VALUES ('1', '1', '5');
+INSERT INTO heroku_37da5348cc1f7c7.rated (userid, movieid, rating) VALUES ('1', '2', '5');
+INSERT INTO heroku_37da5348cc1f7c7.rated (userid, movieid, rating) VALUES ('1', '3', '5');
+INSERT INTO heroku_37da5348cc1f7c7.rated (userid, movieid, rating) VALUES ('1', '4', '5');
