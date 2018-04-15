cdbb509275f19641109fa824fe91af057cb3fd6e
bryant100000 <bhzhao2@illinois.edu>
2018-04-15 18:34:12 -0500

    merging for request

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from wtforms import StringField, SubmitField, SelectField, RadioField, FieldList
 import objects as obj
 import recommender as reco
 import grabMovieInfo as movieGetter
-import math
 import MySQLdb
 import pandas as pd
 import numpy as np
@@ -360,87 +359,6 @@ def viewMovieInfo():
     sqlStr = "SELECT title, releaseYear FROM moviedata WHERE movieid = %s"
     cur.execute(sqlStr, [movieid])
     return render_template('homepage.html', form=form, username=username)
-
-@app.route('/requestRating', methods=['GET', 'POST'])
-def requestRating():
-    if 'username' not in session:
-        return redirect(url_for('homepage'))
-    _userid = session['userid']
-    form = MovieQueryForm()
-    _targetMovie = request.form['movieId']
-
-    conn = getConnection()
-    cur = conn.cursor()
-
-    predictedScore = 0
-    denom = 0
-
-    sqlStr = "SELECT * FROM rated WHERE userid = %s"
-    cur.execute(sqlStr, [_userid])
-    userAvg = 0
-    userCt = 0
-
-    for rating in cur.fetchall():
-        userAvg = userAvg + rating[2]
-        userCt = userCt + 1
-    userAvg = userAvg / userCt  # Determine the user's average rating
-    print(userAvg) #TODO
-    sqlStr = "SELECT * FROM users WHERE userid <> %s"
-    cur.execute(sqlStr, [_userid])
-
-    otherUsers = cur.fetchall()
-    for otherUser in otherUsers:  # Go through each other user in the database
-        _otherid = otherUser[0]
-        otherTargetRating = 0
-
-        sqlStr = "SELECT * FROM rated WHERE userid = %s AND movieid = %s"
-        cur.execute(sqlStr, (_otherid, _targetMovie))
-        if cur.rowcount == 0:
-            continue  # We are only interested in others who have rated this movie
-        else:
-            otherTargetRating = cur.fetchone()[2]
-
-        sqlStr = "SELECT * FROM rated WHERE userid = %s"
-        cur.execute(sqlStr, [_otherid])
-
-        otherAvg = 0
-        otherCt = 0
-        for rating in cur.fetchall():
-            otherAvg = otherAvg + rating[2]
-            otherCt = otherCt + 1
-        otherAvg = otherAvg / otherCt  # Determine the other user's average rating
-
-        sqlStr = "SELECT * FROM rated WHERE userid = %s AND movieid IN (SELECT movieid FROM rated WHERE userid = %s)"
-        cur.execute(sqlStr, (_userid, _otherid))  # Find the movies that both users liked
-        sumProductDifferences = 0
-        sumSquaredDiffUser = 0
-        sumSquaredDiffOther = 0
-
-        for rating in cur.fetchall():
-            sharedMovie = rating[1]
-            userRating = rating[2]
-            otherRating = 0
-
-            sqlStr = "SELECT * FROM rated WHERE userid = %s AND movieid = %s"
-            cur.execute(sqlStr, (_otherid, sharedMovie))
-            otherRating = cur.fetchone()[2]
-
-            sumProductDifferences = diffSquareAdder(sumProductDifferences, userRating, userAvg, otherRating, otherAvg)
-            sumSquaredDiffUser = diffSquareAdder(sumSquaredDiffUser, userRating, userAvg, userRating, userAvg)
-            sumSquaredDiffOther = diffSquareAdder(sumSquaredDiffOther, otherRating, otherAvg, otherRating, otherAvg)
-
-        pearsonCoeff = sumProductDifferences / math.sqrt(sumSquaredDiffUser * sumSquaredDiffOther)
-        if pearsonCoeff != 0:
-            predictedScore = predictedScore + ((otherTargetRating - otherAvg) * pearsonCoeff)
-            denom = denom + pearsonCoeff
-
-    if predictedScore != 0:
-        predictedScore = userAvg + (predictedScore / denom)  # Calculate the total predicted scoree bassed on deviations
-    return str(predictedScore) # TODO Replace me
-
-
-def diffSquareAdder(sumarg, rating1, avg1, rating2, avg2):
-    return sumarg + ((rating1 - avg1) * (rating2 - avg2))
 
 
 if __name__ == "__main__":
